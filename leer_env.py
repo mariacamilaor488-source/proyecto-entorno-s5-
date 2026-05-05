0c131f8095f9ea8826ff4d2e20e740f49c2655c9
Camila Orozco <mariaoropi@unisabana.edu.co>
2026-05-05 18:16:34 -0500

    config inicial

---
+++ leer_env.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+database = os.getenv("DATABASE_URL")
+debug = os.getenv("DEBUG")
+env = os.getenv("ENVIRONMENT")
+
+print(f"Base de datos: {database}")
+print(f"Modo debug: {debug}")
+print(f"Ambiente: {env}")
